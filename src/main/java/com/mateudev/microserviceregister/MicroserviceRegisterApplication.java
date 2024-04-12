67b8da3763defc5067465c26e5941cb209c2094f
Mateus Bertazzo <mateusbataglin52@gmail.com>
2024-04-12 16:00:15 -0300

    Feat: Setup inicial, First Commit

---
+++ src/main/java/com/mateudev/microserviceregister/MicroserviceRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.mateudev.microserviceregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceRegisterApplication.class, args);
+	}
+
+}
