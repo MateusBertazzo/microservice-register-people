cd013e946e19247e47202ab25149774ac9ebcee9
Mateus Bertazzo <mateusbataglin52@gmail.com>
2024-04-12 19:52:37 -0300

    Feat: Cria Dtos para Request e Response

--- src/main/java/com/mateudev/microserviceregister/dto/OfferRequestDto.java
+++ src/main/java/com/mateudev/microserviceregister/dto/OfferRequestDto.java
@@ -1,4 +1,24 @@
 package com.mateudev.microserviceregister.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OfferRequestDto {
+
+    private String name;
+
+    private String lastName;
+
+    private String cpf;
+
+    private String telephone;
+
+    private Double income;
+
+    private int paymentTerm;
+
+    private Double requestedAmount;
 }
--- src/main/java/com/mateudev/microserviceregister/dto/OfferResponseDto.java
+++ src/main/java/com/mateudev/microserviceregister/dto/OfferResponseDto.java
@@ -1,4 +1,32 @@
 package com.mateudev.microserviceregister.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OfferResponseDto {
+
+    private Long id;
+    private String name;
+
+    private String lastName;
+
+    private String cpf;
+
+    private String telephone;
+
+    private Double income;
+
+    private int paymentTerm;
+
+    private Double requestedAmount;
+
+    private Boolean approved;
+
+    private String observation;
 }
